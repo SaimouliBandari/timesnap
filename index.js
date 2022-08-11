2b46dd9f8a1ba3dded5f0708c8b2ef4e22355d74
SaimouliBandari <saimouli4k@gmail.com>
2022-08-11 22:58:09 +0530

    commit 1

--- index.js
+++ index.js
@@ -23,30 +23,37 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get("/api/:date?", (req, res) => {
-  const givenDate = req.params.date;
-  let date;
+// app.get("/api/:date?", (req, res) => {
+//   const givenDate = req.params.date;
+//   let date;
 
-  // check if no date provided
-  if (!givenDate) {
-    date = new Date();
-  } else {
-    // check if unix time:
-    //    number string multiplied by 1 gives this number, data string gives NaN
-    const checkUnix = givenDate * 1;
-    date = isNaN(checkUnix) ? new Date(givenDate) : new Date(checkUnix);
-  }
+//   // check if no date provided
+//   if (!givenDate) {
+//     date = new Date();
+//   } else {
+//     // check if unix time:
+//     //    number string multiplied by 1 gives this number, data string gives NaN
+//     const checkUnix = givenDate * 1;
+//     date = isNaN(checkUnix) ? new Date(givenDate) : new Date(checkUnix);
+//   }
 
-  //check if valid format
-  if (date == "Invalid Date") {
-    res.json({ error: "Invalid Date" });
-  } else {
-    const unix = date.getTime();
-    const utc = date.toUTCString();
-    res.json({ unix, utc });
-  }
+//   //check if valid format
+//   if (date == "Invalid Date") {
+//     res.json({ error: "Invalid Date" });
+//   } else {
+//     const unix = date.getTime();
+//     const utc = date.toUTCString();
+//     res.json({ unix, utc });
+//   }
+// });
+
+app.get('/api/whoami', (req, res) => {
+
+  var ip_add = req.ip;
+  var lan = req.headers['accept-language'];
+  var os = req.headers['user-agent'];
+  res.send({ip: ip_add, language: lan, software: os});
 });
-
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
